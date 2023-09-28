6c6ae87fb7fe553fc3dd70aed5b3b0733d0733e9
jimga150 <jimga150@users.noreply.github.com>
2023-09-27 21:17:40 -0400

    add code name generation

--- register.h
+++ register.h
@@ -45,8 +45,46 @@ public:
     std::vector<BitField> bitfields;
 
     void generate_code_name(){
+        //copy name from human readable name
+        this->code_name = this->name;
 
+        //replace all spaces with underscores
+        char toReplace = ' ';
+        while(replace_first(this->code_name, std::string(1, toReplace), "_"));
+
+        //remove anything thats not an underscore, number, or letter
+        ++toReplace;
+        for (; toReplace < '0'; ++toReplace){
+            while(replace_first(this->code_name, std::string(1, toReplace), ""));
+        }
+        for (toReplace = '9' + 1; toReplace < 'A'; ++toReplace){
+            while(replace_first(this->code_name, std::string(1, toReplace), ""));
+        }
+        for (toReplace = 'Z' + 1; toReplace < 'a'; ++toReplace){
+            if (toReplace == '_') continue;
+            while(replace_first(this->code_name, std::string(1, toReplace), ""));
+        }
+        for (toReplace = 'z' + 1; toReplace < 0x7F; ++toReplace){
+            while(replace_first(this->code_name, std::string(1, toReplace), ""));
+        }
     }
+
+    uint32_t get_byte_len(){
+        float frac_byte_cnt = ((float)bit_len)/8.0;
+        float whole_byte_cnt = ceil(frac_byte_cnt);
+        return (uint32_t)whole_byte_cnt;
+    }
+
+    uint32_t get_default_val(){
+        uint32_t ans = 0;
+        for (BitField& bf : this->bitfields){
+            ans &= bf.default_val << bf.low_index;
+        }
+        return ans;
+    }
+
+    //TODO: derive reset domains with reset domains of bit fields
+    //or maybe assume that resets apply to entire registers?
 };
 
 #endif // REGISTER_H
