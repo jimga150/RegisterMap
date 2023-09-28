f09805aa0bbd567cf27314c7b88bbb42b25abfd8
jimga150 <jimbosned@gmail.com>
2023-09-27 23:19:45 -0400

    move generate_code_name to static fxn

--- register.h
+++ register.h
@@ -10,7 +10,7 @@
 //replaces the first instance of toReplace within s with the string replaceWith
 //returns
 //  true if replacement was succesful
-//  false if replacement was unsuccessful
+//  false if replacement was unsuccessful (nothing happened)
 static bool replace_first(std::string& s, const std::string& toReplace, const std::string& replaceWith){
     std::size_t pos = s.find(toReplace);
     if (pos == std::string::npos) return false;
@@ -28,6 +28,32 @@ static bool replace_first(std::string& s, const std::string& toReplace, const st
     return true;
 }
 
+static std::string generate_code_name(std::string name){
+    //copy name from human readable name
+    std::string code_name = name;
+
+    //replace all spaces with underscores
+    char toReplace = ' ';
+    while(replace_first(code_name, std::string(1, toReplace), "_"));
+
+    //remove anything thats not an underscore, number, or letter
+    ++toReplace;
+    for (; toReplace < '0'; ++toReplace){
+        while(replace_first(code_name, std::string(1, toReplace), ""));
+    }
+    for (toReplace = '9' + 1; toReplace < 'A'; ++toReplace){
+        while(replace_first(code_name, std::string(1, toReplace), ""));
+    }
+    for (toReplace = 'Z' + 1; toReplace < 'a'; ++toReplace){
+        if (toReplace == '_') continue;
+        while(replace_first(code_name, std::string(1, toReplace), ""));
+    }
+    for (toReplace = 'z' + 1; toReplace < 0x7F; ++toReplace){
+        while(replace_first(code_name, std::string(1, toReplace), ""));
+    }
+    return code_name;
+}
+
 class Register
 {
 public:
@@ -43,31 +69,6 @@ public:
     std::string description;
 
     std::vector<BitField> bitfields;
-
-    void generate_code_name(){
-        //copy name from human readable name
-        this->code_name = this->name;
-
-        //replace all spaces with underscores
-        char toReplace = ' ';
-        while(replace_first(this->code_name, std::string(1, toReplace), "_"));
-
-        //remove anything thats not an underscore, number, or letter
-        ++toReplace;
-        for (; toReplace < '0'; ++toReplace){
-            while(replace_first(this->code_name, std::string(1, toReplace), ""));
-        }
-        for (toReplace = '9' + 1; toReplace < 'A'; ++toReplace){
-            while(replace_first(this->code_name, std::string(1, toReplace), ""));
-        }
-        for (toReplace = 'Z' + 1; toReplace < 'a'; ++toReplace){
-            if (toReplace == '_') continue;
-            while(replace_first(this->code_name, std::string(1, toReplace), ""));
-        }
-        for (toReplace = 'z' + 1; toReplace < 0x7F; ++toReplace){
-            while(replace_first(this->code_name, std::string(1, toReplace), ""));
-        }
-    }
 
     uint32_t get_byte_len(){
         float frac_byte_cnt = ((float)bit_len)/8.0;
